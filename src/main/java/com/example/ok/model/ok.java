abda01f8992a25fdff24121760a712174ab5acb8
High Lam <hhighlam2004@gmail.com>
2024-09-10 21:04:40 +0700

    ok 123

--- src/main/java/com/example/ok/model/ok.java
+++ src/main/java/com/example/ok/model/ok.java
@@ -2,4 +2,12 @@ package com.example.ok.model;
 
 public class ok {
     private int id;
+    private String title;
+    private String content;
+    public ok(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+
+    }
 }
